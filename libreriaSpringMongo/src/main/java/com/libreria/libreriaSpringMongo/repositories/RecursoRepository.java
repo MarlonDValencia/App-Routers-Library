5f2d234ea0548fb7924c399206bd0e1911f305b5
MarlonDValencia <marlondvalencia@gmail.com>
2022-01-12 17:38:47 -0500

    Proyecto reactive librery copiado

--- libreriaSpringMongo/src/main/java/com/libreria/libreriaSpringMongo/repositories/RecursoRepository.java
+++ libreriaSpringMongo/src/main/java/com/libreria/libreriaSpringMongo/repositories/RecursoRepository.java
@@ -1,8 +1,8 @@
 package com.libreria.libreriaSpringMongo.repositories;
 
-import com.libreria.libreriaSpringMongo.collections.Recurso;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import com.libreria.libreriaSpringMongo.collections.Recurso; 
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
-public interface  RecursoRepository extends MongoRepository<Recurso, String> {
+public interface  RecursoRepository extends ReactiveMongoRepository<Recurso, String> {
     Recurso findByName(String name);
 }
